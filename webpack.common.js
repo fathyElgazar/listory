6203023f80b6d2e62c13d49923c64ac767938635
fathyElgazar <gazzarDev@gmail.com>
2024-03-05 14:07:28 +0200

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/JS/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     { from: 'src/images', to: 'images' }, // Copy src/images to dist/images
+    //   ],
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
