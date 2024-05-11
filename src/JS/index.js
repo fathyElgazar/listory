7f4dd948d1b9ad56515b2054eeed9c1e41094853
fathyElgazar <gazzarDev@gmail.com>
2024-05-11 18:48:08 +0300

    refactor: Move to it's own module

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,17 +1,18 @@
 import { differenceInQuartersWithOptions } from 'date-fns/fp';
 import '../sass/main.scss';
 import { de, gd } from 'date-fns/locale';
-
-let projects = {};
+import { handleProjectSubmission, renderProject } from './projects';
 
 const btnAddTodo = document.getElementById('btn-add-todo');
 const formContainer = document.getElementById('form-container');
 const btnCloseForm = document.getElementById('close-form');
+const projectsContainer = document.querySelector('.user-projects');
 
 function showForm() {
   formContainer.classList.toggle('show');
 }
 
+renderProject();
 btnAddTodo.addEventListener('click', showForm);
 
 btnCloseForm.addEventListener('click', showForm);
@@ -21,60 +22,18 @@ const btnAddProject = document.getElementById('btn-add-project');
 const proName = document.querySelector('.project-input');
 const userProjectsList = document.querySelector('.user-projects');
 
-// Get all projects
-function getProjects() {
-  const defaultProjects = document.querySelectorAll('.projects__default');
-  let userProjects = document.querySelectorAll('.projects__user');
-  let allProjects = [...defaultProjects, ...userProjects];
-  return allProjects;
-}
-
-// Handle project name in the main side
-function renderProject() {
-  let projects = getProjects();
-  let projectName = projects.forEach((project) => {
-    project.addEventListener('click', (e) => {
-      projectTitle.innerText = project.innerText;
-    });
-  });
-}
-
-renderProject();
-
-// Handle project submission
-function handleProjectSubmission(e) {
-  e.preventDefault();
-  submitProject();
-  renderProject();
-}
-
 btnAddProject.addEventListener('click', handleProjectSubmission);
 
 proName.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
-    // Prevent default form submission if Enter is pressed within the input field
     handleProjectSubmission(e);
   }
 });
 
-function submitProject() {
-  proName.value.trim();
-  if (!proName.value) return;
-  let project = document.createElement('li');
-  project.innerHTML = `
-  <button class="projects__user">
-  <svg class="icon icon-user">
-  <use xlink:href="images/sprite.svg#icon-folder"></use>
-  </svg>
-  ${proName.value}
-  <svg class="icon icon-delete">
-  <use
-    xlink:href="images/sprite.svg#icon-circle-with-minus"
-  ></use>
-</svg>
-  </button>
-  
-  `;
-  userProjectsList.insertAdjacentElement('beforeend', project);
-  proName.value = '';
-}
+export {
+  projectTitle,
+  proName,
+  userProjectsList,
+  btnAddProject,
+  projectsContainer,
+};
